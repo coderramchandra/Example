f4bb678a9f5676420922e9d47be89fd7fb44774a
coderramchandra <ramm2kk@gmail.com>
2023-12-14 13:44:16 +0530

    create a person class

---
+++ src/com/jsp/Example/Person.java
@@ -0,0 +1,9 @@
+package com.jsp.Example;
+
+public class Person {
+	public static void main(String[] args) {
+		int age=20;
+		String name="Ramchandra";
+		System.out.println("Age: "+age+" Name: "+name);
+	}
+}
