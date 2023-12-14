757bb3d022f95566feabc3f2712c58502fd1f196
coderramchandra <ramm2kk@gmail.com>
2023-12-14 13:45:56 +0530

    add money attribute

--- src/com/jsp/Example/Person.java
+++ src/com/jsp/Example/Person.java
@@ -4,6 +4,7 @@ public class Person {
 	public static void main(String[] args) {
 		int age=20;
 		String name="Ramchandra";
-		System.out.println("Age: "+age+" Name: "+name);
+		int money=20000;
+		System.out.println("Age: "+age+" Name: "+name+" Money: "+money);
 	}
 }
